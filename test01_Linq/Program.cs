d89a8d107c9b013296c2b1a85045aa93d531d3fb
ldy <1417245035@qq.com>
2020-02-16 10:03:56 +0800

    0.1.1

---
+++ test01_Linq/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01_Linq
+{
+    public class MartialArtsMaster
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Menpai { get; set; }
+
+        public string Kongfu { get; set; }
+
+        public int Level { get; set; }
+    }
+
+    class Kongfu
+    {
+        public int KongfuId { get; set; }
+
+        public string KongfuName { get; set; }
+
+        public int Lethality { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //初始化武林高手
+            var master = new List<MartialArtsMaster>()
+            {
+                new MartialArtsMaster(){Id = 1,Name = "黄蓉",Age = 8,Menpai = "丐帮",Kongfu = "打狗棒法",Level = 9 },
+                new MartialArtsMaster(){Id = 2,Name = "洪七公",Age = 70,Menpai = "丐帮",Kongfu = "打狗棒法",Level = 10 },
+                new MartialArtsMaster(){Id = 3,Name = "郭靖",Age = 22,Menpai = "丐帮",Kongfu = "降龙十八掌",Level = 10 },
+                new MartialArtsMaster(){Id = 4,Name = "任我行",Age = 50,Menpai = "明教",Kongfu = "葵花宝典",Level = 1 },
+                new MartialArtsMaster(){Id = 5,Name = "东方不败",Age = 35,Menpai = "明教",Kongfu = "葵花宝典",Level = 10 },
+                new MartialArtsMaster(){Id = 6,Name = "林平之",Age = 23,Menpai = "华山",Kongfu = "葵花宝典",Level = 7 },
+                new MartialArtsMaster(){Id = 7,Name = "岳不群",Age = 50,Menpai = "华山",Kongfu = "葵花宝典",Level = 9 }
+            };
+
+            //初始化武学
+            var Kongfu = new List<Kongfu>()
+            {
+                new Kongfu(){KongfuId = 1, KongfuName = "打狗棒法" , Lethality = 90},
+                new Kongfu(){KongfuId = 2, KongfuName = "降龙十八掌" , Lethality = 95},
+                new Kongfu(){KongfuId = 3, KongfuName = "葵花宝典" , Lethality = 100}
+            };
+
+
+            //在武林大侠中选出Level大于8级且门派为丐帮的大侠
+            var sequence = from a in master
+                           where a.Level > 8 && a.Menpai == "丐帮"
+                           select a;
+            var sequenceMethod = master.Where(x => x.Level > 8 && x.Menpai == "丐帮");
+            //过滤所学武功杀伤力大于90的大侠，且按等级进行升序排序。
+            var sequence1 = (from a in master
+                            from b in Kongfu
+                            where b.Lethality > 90 && a.Kongfu == b.KongfuName
+                            orderby a.Level
+                            select new
+                            {
+                                a.Id,
+                                a.Name,
+                                a.Kongfu,
+                                a.Level,
+                                a.Menpai,
+                            });
+            var sequence1Method = master.SelectMany(k => Kongfu, (m, k) => new { mt = m, kf = k })
+                .Where(x => x.kf.Lethality > 90 && x.kf.KongfuName == x.mt.Kongfu)
+                .OrderBy(m => m.mt.Level)
+                .Select(m => new
+                {
+                    m.mt.Id,
+                    m.mt.Name,
+                    m.mt.Kongfu,
+                    m.mt.Level,
+                    m.mt.Menpai,
+                });
+
+            //武林排行榜
+            int i = 0;
+            var sequence2 = from m in master
+                            join k in Kongfu on m.Kongfu equals k.KongfuName
+                            //from k in Kongfu
+                            //where m.Kongfu == k.KongfuName
+                            orderby m.Level * k.Lethality descending, m.Age descending, m.Name
+                            select new
+                            {
+                                m.Name,
+                                m.Id,
+                                m.Kongfu,
+                                weili = m.Level * k.Lethality,
+                                paiming = ++i
+                            };
+            var sequence2Method = master.SelectMany(m => Kongfu, (m, k) => new { mt = m, kf = k }).Where(m => m.mt.Kongfu == m.kf.KongfuName).OrderByDescending(x => x.kf.Lethality * x.mt.Level).ThenByDescending(x => x.mt.Age).Select(
+                x => new { 
+                    x.mt.Id,
+                    x.mt.Name,
+                    V = x.mt.Level *x.kf.Lethality,
+                    x.mt.Kongfu,
+                    paimign = ++i
+                });
+
+            var sequenceM = from m in master
+                            where m.Level > 8
+                            orderby m.Level descending
+                            select m;
+            var sequenceK = from k in Kongfu
+                            where k.Lethality > 90
+                            orderby k.Lethality descending
+                            select k;
+
+            var sequence3 = from m in sequenceM
+                            from k in sequenceK
+                            where m.Kongfu == k.KongfuName
+                            //join k in Kongfu on m.Kongfu equals k.KongfuName
+                            orderby m.Level * k.Lethality descending
+                            select new
+                            {
+                                paiming = ++i,
+                                m.Id,
+                                m.Name,
+                                m.Age,
+                                V = m.Level * k.Lethality
+                            };
+
+            var sequence4 = from k in Kongfu
+                            join m in master on k.KongfuName equals m.Kongfu 
+                            into groups
+                            orderby groups.Count() descending
+                            select new
+                            {
+                                V = ++i,
+                                k.KongfuId,
+                                k.KongfuName,
+                                V1 = groups.Count()
+                            };
+            //Intersect:相等比较器，生成两个序列的交集
+            var sequence5 = (from m in master
+                             where m.Menpai == "明教" || m.Menpai == "华山"
+                             select m).Intersect(from m in master where m.Kongfu == "葵花宝典" select m);
+            foreach (var item in sequence4)
+            {
+                Console.WriteLine(item);
+            }
+            return;
+            string[] languane = { "java", "C#", "C++", "python", "golang" };
+            var query = from a in languane
+                        group a by a.Length into g
+                        orderby g.Key
+                        select g;
+            foreach (var items in query)
+            {
+                Console.WriteLine(items.Key);
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
--- 测试用例空间/Program.cs
+++ 测试用例空间/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace 测试用例空间
 {
@@ -74,7 +73,7 @@ namespace 测试用例空间
         /// <returns></returns>
         public static IEnumerable<string> Select(this IEnumerable<int> source,Func<int,string> selector)
         {
-            foreach (int item in source)
+            foreach (var item in source)
             {
                 yield return selector(item);
             }
@@ -92,6 +91,94 @@ namespace 测试用例空间
                 yield return selector(item);
             }
         }
+        /// <summary>
+        /// 实现了Any的重载，确定一个序列是否包含一个值,返回bool类型。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static bool Any<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            //调用where的方法判断序列是否有值并返回此值，这样调用他的子类就可以确定次序列是否有此项。
+            return source.Where(predicate).GetEnumerator().MoveNext();
+        }
+        /// <summary>
+        /// 实现count的核心
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static int Count<T>(this IEnumerable<T> source)
+        {
+            var count = 0;
+            while (source.GetEnumerator().MoveNext()!=false)
+            {
+                count++;
+            }
+
+            return count;
+        }
+        /// <summary>
+        /// 实现count的函数重载
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static int Count<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return source.Where(predicate).Count();
+        }
+        /// <summary>
+        /// 简单版本的实现求和
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static int Sum(this IEnumerable<int> source)
+        {
+            int sum = 0;
+            foreach (var item in source)
+            {
+                sum = sum + item;
+            }
+
+            return sum;
+        }
+        /// <summary>
+        /// 种子求和，可以实现一个固定的源
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="seed"></param>
+        /// <returns></returns>
+        public static int Sum(this IEnumerable<int> source,int seed)
+        {
+            int sum = seed;
+            foreach (var item in source)
+            {
+                sum = sum + item;
+            }
+            return sum;
+        }
+        /// <summary>
+        /// 实现一个累加器的方法，这种方式更加的通用，对于任意类型都可以实现特的函数方式的累加。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">方法调用者</param>
+        /// <param name="func">一个方法函数</param>
+        /// <returns></returns>
+        public static T Aggregate<T>(this IEnumerable<T> source, Func<T, T, T> func)
+        {
+            var enumerator = source.GetEnumerator();
+            enumerator.MoveNext();
+            var sum = enumerator.Current;
+            while (enumerator.MoveNext())
+            {
+                sum = func(sum, enumerator.Current);
+            }
+            return sum;
+        }
+
 
     }
 
@@ -99,8 +186,37 @@ namespace 测试用例空间
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(SequenceFormConsole().Aggregate((x, y) => x + "," + y));
+            return;
+            //Linq查询集语法
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] b = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            var sum = from x in a
+                      from y in b
+                      select new { x, y, sum = x + y };
+            foreach (var ss in sum)
+            {
+                Console.WriteLine(ss);
+            }
+            
+            return;
+            //OrderBy(排序)方法:这里的orderby与thenby方法并不会具体实现，这种排序方法实际上非常复杂，你可以尝试去阅读一些源码，这里索
+            //要知道的只有 1、排序方法非常消耗计算机资源，不只是C#，在java，object-c等等语言上都是这样，但是这种消耗的资源我们会尽量的
+            //去少消耗 2、了解一些排序的原理，这里的排序不会去根据不同的lambda函数一次一次的遍历序列，直至返回排序的序列，而是等待着，根
+            //根据所有的lambda函数，去中和出一个满足所有lambda函数的排序规则，一次性的遍历出满足所有的lamabd函数的序列并返回，这种方式
+            //无疑是效率很高的一种方法，不必去多次排序序列。3、合理使用OrderBy与ThenBy。
+            var sequences = SequenceFormConsole().OrderBy(x => x.Length).ThenBy(x => x);
+            foreach (var item in sequences)
+            {
+                Console.WriteLine(item);
+            }
+            return;
+            
+            //count方法
+            Console.WriteLine(SequenceFormConsole().Count(x=>x.Contains("hello")));
+            return;
             //any方法
-            Console.WriteLine(SequenceFormConsole().Any());
+            Console.WriteLine(SequenceFormConsole().Any(x=>x=="hello"));
 
             return;
             var input = SequenceFormConsole().Select(x => int.Parse(x));
